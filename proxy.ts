0a88657dd7a1c286905fd6266c9e75c81970f277
oksanA016 <frunzeoksana86@gmail.com>
2026-03-06 19:58:38 +0200

    fix proxy

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,13 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { cookies } from "next/headers";
 import { checkSession } from "@/lib/api/serverApi";
 
 export async function proxy(request: NextRequest) {
-  const accessToken = request.cookies.get("accessToken")?.value;
-  const refreshToken = request.cookies.get("refreshToken")?.value;
+  const cookieStore = await cookies();
+
+  const accessToken = cookieStore.get("accessToken")?.value;
+  const refreshToken = cookieStore.get("refreshToken")?.value;
 
   const isPrivateRoute =
     request.nextUrl.pathname.startsWith("/profile") ||
@@ -32,11 +35,11 @@ export async function proxy(request: NextRequest) {
           ? setCookieHeader
           : [setCookieHeader];
 
-        cookiesArray.forEach((cookie: string) => {
+        cookiesArray.forEach((cookie) => {
           const [nameValue] = cookie.split(";");
           const [name, value] = nameValue.split("=");
 
-          res.cookies.set(name, value);
+          cookieStore.set(name, value);
         });
       }
 
